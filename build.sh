a291b689da3d690ddfe1b524bc68239b767f9384
solder3t <31514860+solder3t@users.noreply.github.com>
2026-09-18 20:50:01 +0000

    chore: update Antigravity versions

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ NC='\033[0m' # No Color
 OUTDIR="${OUTDIR:-$HOME/rpkg}"
 
 FALLBACK_VERSION_IDE="2.5.5"
-FALLBACK_VERSION_AGENT="2.14.0"
+FALLBACK_VERSION_AGENT="2.15.0"
 
 FALLBACK_URL_IDE_X64="https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/2.5.5-4923483625488384/linux-x64/Antigravity%20IDE.tar.gz"
-FALLBACK_URL_AGENT_X64="https://storage.googleapis.com/antigravity-public/antigravity-hub/2.14.0-5449404535144448/linux-x64/Antigravity.tar.gz"
+FALLBACK_URL_AGENT_X64="https://storage.googleapis.com/antigravity-public/antigravity-hub/2.15.0-6576870427328512/linux-x64/Antigravity.tar.gz"
 
 RESOLVED_VERSION_IDE=""
 RESOLVED_URL_IDE_X64=""
